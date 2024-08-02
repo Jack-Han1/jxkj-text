d0c2afb9ab28da486b947698eff40457b1abe03e
李欣桐 <JavaCoffee123@noreply.gitcode.com>
2024-08-02 15:59:50 +0800

    sql

--- sql/magazine_table.sql
+++ sql/magazine_table.sql
@@ -16,3 +16,7 @@ create table magazine
  INSERT INTO jingxin.magazine (title, text, date, url, img, status, sort, update_time) 
  VALUES (' 精信科技总裁李文帅出席武汉市高新技术产业会...', '6月25日，由武汉市人民政府主办的武汉光谷高新技术产业合作大会在武汉举行。会签武汉市市委书记...',
   '2023-06-25', 'https://s.dps.cn/go/f691afe0322c6e6366088e83d4d81267', 'https://sf.dps.cn/thn/202406/f691afe0322c6e6366088e83d4d81267.jpg', 1, 0, '2024-07-31 21:25:20');
+
+
+  alter table article
+    add clicks int default 0 not null comment '点击量';
